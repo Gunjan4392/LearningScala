be954aaff8656fbe8a9655aab120a4f662cedc84
Gunjan4392 <gunjankhandelwal4392@gmail.com>
2019-06-24 03:50:26 -0700

    Update scalaExamples1.scala

--- scalaExamples1.scala
+++ scalaExamples1.scala
@@ -1,11 +1,33 @@
 //Print the greater number
-val c=(math.random * 100).toInt
-				println("Value of c = " + c)
-				val d=(math.random * 100).toInt
-				println("Value of d = " + d)
-				if (c>d)
-					println(c + " is greater than " + d)
-				else if (c==d)
-					println("Both are equal")
-				else if (c<d)
-					println(c + " is smaller than " + d)
+		val c=(math.random * 100).toInt
+		println("Value of c = " + c)
+		val d=(math.random * 100).toInt
+		println("Value of d = " + d)
+		if (c>d)
+			println(c + " is greater than " + d)
+		else if (c==d)
+			println("Both are equal")
+		else if (c<d)
+			println(c + " is smaller than " + d)
+
+//Factorial of a number
+	var res =1
+	val a = 10
+	for (e <- a to 2 by -1)
+		res = res * e
+		println("Factorial of "+ a + " is "+ res)
+
+//Fibonacci Series of a number
+	val a = 10
+	var pre = 0
+	var cur = 1
+	println(pre)
+	println(cur)
+	var res = 0
+	for (e <- 2 to a -1)
+	{
+		res = pre + cur
+		println (res)
+		pre =cur
+		cur =res
+	}
