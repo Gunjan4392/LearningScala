9241271963309e9edb1c5197bb0eaaf551dbecf0
Gunjan4392 <gunjankhandelwal4392@gmail.com>
2019-06-24 03:19:42 -0700

    Create scalaExamples1.scala

---
+++ scalaExamples1.scala
@@ -0,0 +1,11 @@
+//Print the greater number
+val c=(math.random * 100).toInt
+				println("Value of c = " + c)
+				val d=(math.random * 100).toInt
+				println("Value of d = " + d)
+				if (c>d)
+					println(c + " is greater than " + d)
+				else if (c==d)
+					println("Both are equal")
+				else if (c<d)
+					println(c + " is smaller than " + d)
